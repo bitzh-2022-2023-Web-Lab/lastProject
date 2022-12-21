dd093a0389b7085d34fd2d92488c45117d710fa8
JulienZeng <936636589@qq.com>
2022-12-21 10:43:44 +0800

    添加亮暗主题切换

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { useDark, useToggle } from "@vueuse/core";
+
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
+</script>
+
+<template>
+  <el-switch
+    v-model="isDark"
+    inactive-icon="Sunny"
+    active-icon="Moon"
+  />
+</template>
