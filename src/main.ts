8b023c76d22b6f9b0bad84f43b7f712a911d5733
JulienZeng <936636589@qq.com>
2022-12-21 10:43:16 +0800

    加载element图标库

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from "./router/index"
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import router from "./router/index";
+import "element-plus/theme-chalk/dark/css-vars.css";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+const app = createApp(App);
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
+
+app.use(router);
+app.mount("#app");
