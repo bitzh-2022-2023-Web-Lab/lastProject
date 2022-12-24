dc90a81b2abdaf4a757ae46ffb61104c77352dd9
JulienZeng <936636589@qq.com>
2022-12-24 13:23:01 +0800

    完成主页

---
+++ src/views/HomePage.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import Carousel from '../components/Carousel.vue';
+import PostList from '../components/PostList.vue';
+
+
+</script>
+
+<template>
+  <Carousel></Carousel>
+  <PostList></PostList>
+</template>
