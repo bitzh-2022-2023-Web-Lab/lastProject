c3d711372f46cc25aa10680ca2984261135b0d94
JulienZeng <936636589@qq.com>
2022-12-24 13:21:50 +0800

    feat: 登陆注册合为一个page

---
+++ src/views/AuthPage.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import Login from "../components/login.vue";
+import Register from "../components/register.vue";
+
+import { ref } from "vue";
+import type { TabsPaneContext } from "element-plus";
+
+const activeName = ref("first");
+
+const handleClick = (tab: TabsPaneContext, event: Event) => {
+  console.log(tab, event);
+};
+</script>
+
+<template>
+  <el-tabs v-model="activeName" class="demo-tabs dark"  @tab-click="handleClick">
+    <el-tab-pane label="登录" name="first">
+        <Login></Login>
+    </el-tab-pane>
+    <el-tab-pane label="注册" name="second">
+        <Register></Register>
+    </el-tab-pane>
+  </el-tabs>
+</template>
