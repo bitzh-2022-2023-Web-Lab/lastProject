4a3a4eda2ec9675ebb1a19e5bea66f8b37edefb0
JulienZeng <936636589@qq.com>
2022-12-25 13:20:26 +0800

    挂载组件

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,13 @@
 <script setup lang="ts">
+import HeaderBar from './components/HeaderBar.vue'
 
 </script>
 
 <template>
-  
+  <HeaderBar></HeaderBar>
+  <router-view />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
