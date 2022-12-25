27051c83e8748f6a1a43357f721ad12b315c62f1
JulienZeng <936636589@qq.com>
2022-12-25 15:16:25 +0800

    feat: 添加DailyNote路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,9 +3,10 @@ import HomePage from "../views/HomePage.vue";
 import AuthPage from "../views/AuthPage.vue";
 import UserPage from "../views/UserPage.vue";
 
-import EditUserInfo from "../components/EditUserInfo.vue"
-import ChangePwd from "../components/ChangePwd.vue"
-import HelperSetting from "../components/HelperSetting.vue"
+import EditUserInfo from "../components/EditUserInfo.vue";
+import ChangePwd from "../components/ChangePwd.vue";
+import HelperSetting from "../components/HelperSetting.vue";
+import DailyNote from "../components/DailyNote.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -16,7 +17,7 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: "/auth",
     name: "Auth",
-    component: AuthPage
+    component: AuthPage,
   },
   {
     path: "/user",
@@ -24,22 +25,27 @@ const routes: Array<RouteRecordRaw> = [
     component: UserPage,
     children: [
       {
-        path: '/user/edit',
-        name: 'EditUserInfo',
+        path: "/user/edit",
+        name: "EditUserInfo",
         component: EditUserInfo,
       },
       {
-        path: '/user/changePwd',
-        name: 'ChangePwd',
+        path: "/user/changePwd",
+        name: "ChangePwd",
         component: ChangePwd,
       },
       {
-        path: '/user/setting',
-        name: 'HelperSetting',
+        path: "/user/setting",
+        name: "HelperSetting",
         component: HelperSetting,
       },
-    ]
-  }
+      {
+        path: "/user/dailyNote",
+        name: "DailyNote",
+        component: DailyNote,
+      },
+    ],
+  },
 ];
 
 const router = createRouter({
