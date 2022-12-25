55a48a92e99ea13fc5b839a5144580ca75abc1e0
JulienZeng <936636589@qq.com>
2022-12-25 13:22:39 +0800

    添加路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,46 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import HomePage from "../views/HomePage.vue";
+import AuthPage from "../views/AuthPage.vue";
+import UserPage from "../views/UserPage.vue";
 
-const routes: Array<RouteRecordRaw> = [];
+import EditUserInfo from "../components/EditUserInfo.vue"
+import ChangePwd from "../components/ChangePwd.vue"
+import HelperSetting from "../components/HelperSetting.vue"
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "Home",
+    component: HomePage,
+  },
+  {
+    path: "/auth",
+    name: "Auth",
+    component: AuthPage
+  },
+  {
+    path: "/user",
+    name: "User",
+    component: UserPage,
+    children: [
+      {
+        path: '/user/edit',
+        name: 'EditUserInfo',
+        component: EditUserInfo,
+      },
+      {
+        path: '/user/changePwd',
+        name: 'ChangePwd',
+        component: ChangePwd,
+      },
+      {
+        path: '/user/setting',
+        name: 'HelperSetting',
+        component: HelperSetting,
+      },
+    ]
+  }
+];
 
 const router = createRouter({
   history: createWebHistory(),
