6aa8a6827c72ce877d69fb26bb48d235b2801aea
JulienZeng <936636589@qq.com>
2022-12-25 13:17:03 +0800

    feat: 完成个人中心的布局设计

---
+++ src/views/UserPage.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import UserCard from "../components/UserCard.vue";
+import UserSiderBar from "../components/UserSiderBar.vue";
+</script>
+
+<template>
+  <br />
+  <div style="display: flex; flex-direction: column">
+    <div class="head"><UserCard></UserCard></div>
+    <br />
+    <div style="display: flex; flex-direction: row">
+      <div class="siderBar" style="margin-right: 5px;"><UserSiderBar></UserSiderBar></div>
+      <div style="flex: 1;margin-left: 5px;"><router-view /></div>
+    </div>
+  </div>
+</template>
